792ccb3e1dbab71b6dbe3ab7aeb11bbc6665800a
Sivaraman <dsivaram@ford.com>
2019-11-08 12:13:15 +0530

    Add button controller

--- app/src/main/java/com/learning/cicdsession/MainActivity.kt
+++ app/src/main/java/com/learning/cicdsession/MainActivity.kt
@@ -2,11 +2,18 @@ package com.learning.cicdsession
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMeButton = findViewById<Button>(R.id.clickMe_button)
+
+        clickMeButton.setOnClickListener { Toast.makeText(this, "You clicked Me..", Toast.LENGTH_LONG).show() }
     }
+
 }
